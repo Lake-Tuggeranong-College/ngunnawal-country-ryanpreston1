644cd9fab58bf22fc377f9ee1649c6dc9c47fefc
ryanpreston1 <160078896+ryanpreston1@users.noreply.github.com>
2024-02-16 00:08:14 +0000

    new routes

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Ryan P!'
+    return render_template("index.html", title="Homepage")
 
+@app.route('/history')
+def history():  # put application's code here
+    return render_template("history.html", title="History of Ngunnawal")
 
 if __name__ == '__main__':
     app.run()
