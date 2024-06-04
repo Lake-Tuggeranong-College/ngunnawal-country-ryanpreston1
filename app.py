6a699e8baa8c2631aaea82d27bb888ab18b889dd
Ryan Cather <49006682+RyanCather@users.noreply.github.com>
2024-06-04 15:35:55 +1000

    reorganisation

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from config import Config # type: ignore
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, login_user, LoginManager, logout_user, login_required 
 
 app = Flask(__name__)
+
+app.config.from_object(Config) 
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from forms import ContactForm, LoginForm, RegistrationForm, ResetPasswordForm # type: ignore
+from models import User, Contact # type: ignore
 
 @app.route('/')
 def hello_world():
     return render_template("index.html", title="Homepage")
 
-
-@app.route('/contact')
-def contact():
-    return render_template("contact.html", title="Contact Us") 
-
 @app.route('/gallery')
 def gallery():
     return render_template("gallery.html", title="Photo Gallery")
-
 
 @app.route('/history')
 def history():
@@ -23,19 +28,6 @@ def history():
 @app.route('/grid')
 def grid():
     return render_template("grid.html", title="Bootstrap Grid") 
-
-from flask import Flask, render_template, request, redirect, url_for, flash
-from config import Config # type: ignore
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import current_user, login_user, LoginManager, logout_user, login_required 
-
-app.config.from_object(Config) 
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.login_view = 'login'
-
-from forms import ContactForm, LoginForm, RegistrationForm, ResetPasswordForm, UserProfileForm # type: ignore
-from models import User, Contact # type: ignore
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
@@ -47,8 +39,6 @@ def contact():
         return redirect(url_for("homepage"))
     return render_template("contact.html", title="Contact Us", form=form, user=current_user)
 
-from forms import ContactForm, LoginForm, RegistrationForm # type: ignore
-from models import Contact, User # type: ignore
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -90,6 +80,8 @@ def reset_password():
         flash("Your password has been changed.")
         return redirect(url_for('homepage'))
     return render_template("passwordreset.html", title='Reset Password', form=form, user=current_user)
+
+
 @app.route('/contact_messages')
 @login_required
 def view_contact_messages(): contact_messages = Contact.query.all()
