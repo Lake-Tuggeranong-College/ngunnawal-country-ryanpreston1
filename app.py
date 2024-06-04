a389883723376d0abbd716d030e61e0f7b604ab3
ryanpreston1 <160078896+ryanpreston1@users.noreply.github.com>
2024-06-04 04:35:16 +0000

    new

--- app.py
+++ app.py
@@ -23,3 +23,26 @@ def history():
 @app.route('/grid')
 def grid():
     return render_template("grid.html", title="Bootstrap Grid") 
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+from config import Config # type: ignore
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, login_user, LoginManager, logout_user, login_required 
+
+app.config.from_object(Config) 
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from forms import ContactForm, LoginForm, RegistrationForm, ResetPasswordForm, UserProfileForm # type: ignore
+from models import User, Contact # type: ignore
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        new_contact = Contact(name=form.name.data, email=form.email.data, message=form.message.data)
+        db.session.add(new_contact)
+        db.session.commit()
+        return redirect(url_for("homepage"))
+    return render_template("contact.html", title="Contact Us", form=form, user=current_user)
