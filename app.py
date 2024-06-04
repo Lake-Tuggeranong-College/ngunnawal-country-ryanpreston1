76c2474bd0d5a870f88db885afb59a6aa0f6b90d
ryanpreston1 <160078896+ryanpreston1@users.noreply.github.com>
2024-06-04 05:07:30 +0000

    new

--- app.py
+++ app.py
@@ -46,3 +46,50 @@ def contact():
         db.session.commit()
         return redirect(url_for("homepage"))
     return render_template("contact.html", title="Contact Us", form=form, user=current_user)
+
+from forms import ContactForm, LoginForm, RegistrationForm # type: ignore
+from models import Contact, User # type: ignore
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(email_address=form.email_address.data, name=form.name.data, user_level=1, active=True) # defaults to regular user
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("homepage"))
+    return render_template("registration.html", title="User Registration", form=form)
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=form.email_address.data).first()
+        print(user)
+        if user is not None and user.check_password(form.password.data):
+            # User has been authenticated
+            login_user(user)
+            print("DEBUG: Login Successful")
+            return redirect(url_for("homepage"))
+        else:
+            print("DEBUG: Login Failed")
+            # Username or password incorrect
+            return redirect(url_for("login"))
+    return render_template("login.html", title="Login", form=form)
+
+@app.route('/passwordreset', methods=['GET', 'POST'])
+@login_required
+def reset_password():
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=current_user.email_address).first()
+        user.set_password(form.new_password.data)
+        db.session.commit()
+        flash("Your password has been changed.")
+        return redirect(url_for('homepage'))
+    return render_template("passwordreset.html", title='Reset Password', form=form, user=current_user)
+@app.route('/contact_messages')
+@login_required
+def view_contact_messages(): contact_messages = Contact.query.all()
